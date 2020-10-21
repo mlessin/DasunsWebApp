7f07321e19f61733484d9bf10acecb97fac5a08c
mlessin <roymukunde@gmail.com>
2020-10-20 20:01:49 -0700

    i have put the components of the booking form

--- frontend/src/components/bookingForm.js
+++ frontend/src/components/bookingForm.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import "bootstrap/dist/css/bootstrap.css";
 import Logo from "../../images/Logo.png";
-import "../style.css";
+import "./components/style.css";
 import { Link } from "react-router-dom";
 
 
