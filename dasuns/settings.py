557c5cedf67a1eaa8c602b710743e8d3b67d0155
steven <kawooyastevenug@gmail.com>
2020-11-24 12:12:01 +0300

    added postgres connection

--- dasuns/settings.py
+++ dasuns/settings.py
@@ -80,8 +80,12 @@ WSGI_APPLICATION = 'dasuns.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': "dasuns",
+        'USER': "postgres",
+        'PASSWORD':"Omugezi",
+        'HOST': "localhost",
+        'PORT': "5432",
     }
 }
 
