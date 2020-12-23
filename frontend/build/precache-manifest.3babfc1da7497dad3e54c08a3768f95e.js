b646490615b8354005ac7b6beb01433ed2ecc45a
Irenyak1 <inyakate@gmail.com>
2020-12-23 23:04:45 +0300

    Adds build directory in the frontend

---
+++ frontend/build/precache-manifest.3babfc1da7497dad3e54c08a3768f95e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c0937126a2d004f880f84d28b9a585e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "96a66a2e87ef31d17e51",
+    "url": "/static/css/2.1b35acee.chunk.css"
+  },
+  {
+    "revision": "941d244e3a4c75c02722",
+    "url": "/static/css/main.43223385.chunk.css"
+  },
+  {
+    "revision": "96a66a2e87ef31d17e51",
+    "url": "/static/js/2.0a48efb8.chunk.js"
+  },
+  {
+    "revision": "4188741b4aa7f08c502a78e46dabcf11",
+    "url": "/static/js/2.0a48efb8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "941d244e3a4c75c02722",
+    "url": "/static/js/main.774b4ae0.chunk.js"
+  },
+  {
+    "revision": "92ef4bed96d8c7b376c2",
+    "url": "/static/js/runtime-main.11b51e6e.js"
+  },
+  {
+    "revision": "1cbd7af9414dfff0185cbba2b2957df7",
+    "url": "/static/media/banner.1cbd7af9.jpg"
+  }
+]);
